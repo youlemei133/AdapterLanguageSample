da367f16d49276731ee84645e0a4450c70ebc6fa
hudawei <914493278@qq.com>
2017-02-03 14:38:05 +0800

    Initial commit

---
+++ app/src/main/java/com/chiigu/adapterlanguagesample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.chiigu.adapterlanguagesample;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("MainActivity","当前版本为："+ Build.VERSION.SDK_INT);
+            }
+        });
+    }
+}
